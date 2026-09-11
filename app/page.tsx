5ac02c8167faf21d8813c27a924b6ffe1016d16b
Brijesh Bhaskaran <12258140+brijbh@users.noreply.github.com>
2026-09-11 14:18:51 +0530

    fix: use standard initial scroll behavior

--- app/page.tsx
+++ app/page.tsx
@@ -193,7 +193,7 @@ export default function Home() {
 
   useEffect(() => {
     currentRef.current?.scrollIntoView({
-      behavior: "instant",
+      behavior: "auto",
       block: "nearest",
       inline: "center",
     });
